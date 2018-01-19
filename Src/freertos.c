12222bba66e8a923bce832588cbfd57ddeb867a6
oliver <oliver@leedarson.com>
2018-01-19 18:44:02 +0800

    first commit

---
+++ Src/freertos.c
@@ -0,0 +1,215 @@
+/**
+  ******************************************************************************
+  * File Name          : freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * This notice applies to any and all portions of this file
+  * that are not between comment pairs USER CODE BEGIN and
+  * USER CODE END. Other portions of this file, whether 
+  * inserted by the user or by software development tools
+  * are owned by their respective copyright owners.
+  *
+  * Copyright (c) 2018 STMicroelectronics International N.V. 
+  * All rights reserved.
+  *
+  * Redistribution and use in source and binary forms, with or without 
+  * modification, are permitted, provided that the following conditions are met:
+  *
+  * 1. Redistribution of source code must retain the above copyright notice, 
+  *    this list of conditions and the following disclaimer.
+  * 2. Redistributions in binary form must reproduce the above copyright notice,
+  *    this list of conditions and the following disclaimer in the documentation
+  *    and/or other materials provided with the distribution.
+  * 3. Neither the name of STMicroelectronics nor the names of other 
+  *    contributors to this software may be used to endorse or promote products 
+  *    derived from this software without specific written permission.
+  * 4. This software, including modifications and/or derivative works of this 
+  *    software, must execute solely and exclusively on microcontroller or
+  *    microprocessor devices manufactured by or for STMicroelectronics.
+  * 5. Redistribution and use of this software other than as permitted under 
+  *    this license is void and will automatically terminate your rights under 
+  *    this license. 
+  *
+  * THIS SOFTWARE IS PROVIDED BY STMICROELECTRONICS AND CONTRIBUTORS "AS IS" 
+  * AND ANY EXPRESS, IMPLIED OR STATUTORY WARRANTIES, INCLUDING, BUT NOT 
+  * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY, FITNESS FOR A 
+  * PARTICULAR PURPOSE AND NON-INFRINGEMENT OF THIRD PARTY INTELLECTUAL PROPERTY
+  * RIGHTS ARE DISCLAIMED TO THE FULLEST EXTENT PERMITTED BY LAW. IN NO EVENT 
+  * SHALL STMICROELECTRONICS OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+  * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+  * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+  * OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF 
+  * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING 
+  * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
+  * EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "cmsis_os.h"
+
+
+
+/* USER CODE BEGIN Includes */     
+#include "stm32f1xx_hal.h"
+#include "cmsis_os.h"
+#include "adc.h"
+#include "iwdg.h"
+#include "usart.h"
+#include "gpio.h"
+/* USER CODE END Includes */
+
+/* Variables -----------------------------------------------------------------*/
+osThreadId defaultTaskHandle;
+osThreadId ADC_TaskHandle;
+osThreadId GPS_TaskHandle;
+osThreadId UC20_TaskHandle;
+osThreadId Debug_TaskHandle;
+
+/* USER CODE BEGIN Variables */
+extern uint8_t aTxStartMessage[];
+extern uint8_t aRxBuffer[20];
+
+/* USER CODE END Variables */
+
+/* Function prototypes -------------------------------------------------------*/
+void StartDefaultTask(void const * argument);
+void StartADCTask(void const * argument);
+void StartGPSTask(void const * argument);
+void StartUC20Task(void const * argument);
+void StartDebugTask(void const * argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/* USER CODE BEGIN FunctionPrototypes */
+
+/* USER CODE END FunctionPrototypes */
+
+/* Hook prototypes */
+
+/* Init FreeRTOS */
+
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+       
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* Create the thread(s) */
+  /* definition and creation of defaultTask */
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 64);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+  /* definition and creation of ADC_Task */
+  osThreadDef(ADC_Task, StartADCTask, osPriorityIdle, 0, 128);
+  ADC_TaskHandle = osThreadCreate(osThread(ADC_Task), NULL);
+
+  /* definition and creation of GPS_Task */
+  osThreadDef(GPS_Task, StartGPSTask, osPriorityIdle, 0, 128);
+  GPS_TaskHandle = osThreadCreate(osThread(GPS_Task), NULL);
+
+  /* definition and creation of UC20_Task */
+  osThreadDef(UC20_Task, StartUC20Task, osPriorityIdle, 0, 128);
+  UC20_TaskHandle = osThreadCreate(osThread(UC20_Task), NULL);
+
+  /* definition and creation of Debug_Task */
+  osThreadDef(Debug_Task, StartDebugTask, osPriorityIdle, 0, 64);
+  Debug_TaskHandle = osThreadCreate(osThread(Debug_Task), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+}
+
+/* StartDefaultTask function */
+void StartDefaultTask(void const * argument)
+{
+
+  /* USER CODE BEGIN StartDefaultTask */
+	HAL_GPIO_WritePin(GPS_RESET_GPIO_Port,GPS_RESET_Pin,GPIO_PIN_SET);
+	HAL_GPIO_WritePin(GPS_STANDBY_GPIO_Port,GPS_STANDBY_Pin,GPIO_PIN_SET);
+  /* Infinite loop */
+  for(;;)
+  {
+	HAL_GPIO_WritePin(GPIOA, DEBUG_LED_Pin, GPIO_PIN_SET);
+    osDelay(50);
+	HAL_GPIO_WritePin(GPIOA, DEBUG_LED_Pin, GPIO_PIN_RESET);
+    osDelay(5000);	
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* StartADCTask function */
+void StartADCTask(void const * argument)
+{
+  /* USER CODE BEGIN StartADCTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartADCTask */
+}
+
+/* StartGPSTask function */
+void StartGPSTask(void const * argument)
+{
+  /* USER CODE BEGIN StartGPSTask */
+  /* Infinite loop */
+  for(;;)
+  {
+	//HAL_UART_Transmit_IT(&huart1, (uint8_t *)"TEST", sizeof("TEST"));
+	 // HAL_UART_Receive_IT(&huart1, (uint8_t *)aRxBuffer, 10);
+    osDelay(1000);
+  }
+  /* USER CODE END StartGPSTask */
+}
+
+/* StartUC20Task function */
+void StartUC20Task(void const * argument)
+{
+  /* USER CODE BEGIN StartUC20Task */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartUC20Task */
+}
+
+/* StartDebugTask function */
+void StartDebugTask(void const * argument)
+{
+  /* USER CODE BEGIN StartDebugTask */
+  /* Infinite loop */
+  for(;;)
+  {
+	HAL_IWDG_Refresh(&hiwdg);
+    osDelay(1);
+  }
+  /* USER CODE END StartDebugTask */
+}
+
+/* USER CODE BEGIN Application */
+     
+/* USER CODE END Application */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
